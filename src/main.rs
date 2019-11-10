80f3aeb974508d5f1610a8b3dc90298691fd9d53
uma <blog3170@gmail.com>
2019-11-10 22:31:58 +0900

    save

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,72 @@
-use midir::MidiInput::ports;
 use midir::{Ignore, MidiInput};
-use rodio;
+use rodio::decoder::DecoderError;
+use rodio::{Decoder, Device, Sink};
 use std::error::Error;
-use std::io::{stdin, BufReader};
+use std::io::{stdin, BufReader, Read, Seek};
+
 fn main() {
 	println!("Hello, world!");
 	let device = rodio::default_output_device().unwrap();
-	let sink = rodio::Sink::new(&device);
+	let sink = Sink::new(&device);
+	let file = std::fs::File::open("Processed/High Tom08.wav").unwrap();
+	let out_put_device = OutputDevice::new(device, sink, decoded);
 
-	let file = std::fs::File::open("Processed/High_Tom08.wav").unwrap();
-	sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-
-	sink.sleep_until_end();
+	run(out_put_device);
 }
 
-fn set_device() -> Result<(), Box<dyn Error>> {
+struct OutputDevice<R>
+where
+	R: Read + Seek,
+{
+	device: Device,
+	sink: Sink,
+	high_tom: Decoder<R>,
+}
+
+impl<R> OutputDevice<R>
+where
+	R: Read + Seek,
+{
+	pub fn new(device: Device, sink: Sink, high_tom: Decoder<R>) -> Self {
+		OutputDevice {
+			device,
+			sink,
+			high_tom,
+		}
+	}
+
+	fn play_hige_tom(&self) {
+		self.play(self.high_tom);
+	}
+
+	fn play(&self, sound: Decoder<R>) {
+		let decoded = rodio::Decoder::new(BufReader::new(file)).unwrap();
+		self.sink.append(sound);
+		self.sink.sleep_until_end();
+	}
+}
+
+fn run<R>(out_put_device: OutputDevice<R>) -> Result<(), Box<dyn Error>>
+where
+	R: Read + Seek,
+{
 	let mut input = String::new();
 	let mut midi_in = MidiInput::new("nanoPAD2")?;
 	midi_in.ignore(Ignore::None);
 	// Get an input port (read from console if multiple are available)
 	let in_ports = midi_in.ports();
-	let in_port = match in_ports.len() {
-		0 => return Err("no input port found".into()),
-		1 => {
-			println!(
-				"Choosing the only available input port: {}",
-				midi_in.port_name(&in_ports[0]).unwrap()
-			);
-			&in_ports[0]
+
+	let mut input = String::new();
+	for (i, p) in in_ports.iter().enumerate() {
+		if midi_in.port_name(p).unwrap() == "nanoPAD2" {
+			input = i.to_string();
+			break;
 		}
-		_ => {
-			println!("\nAvailable input ports:");
-			let mut input = String::new();
-			for (i, p) in in_ports.iter().enumerate() {
-				if midi_in.port_name(p).unwrap() == "nanoPAD2" {
-					input = i.to_string();
-					break;
-				}
-			}
-			// print!("Please select input port: ");
-			// stdout().flush()?;
-			// stdin().read_line(&mut input)?;
-			in_ports
-				.get(input.trim().parse::<usize>()?)
-				.ok_or("invalid input port selected")?
-		}
-	};
+	}
+
+	let in_port = in_ports
+		.get(input.trim().parse::<usize>()?)
+		.ok_or("invalid input port selected")?;
 
 	println!("\nOpening connection");
 	let in_port_name = midi_in.port_name(in_port)?;
@@ -56,7 +77,7 @@ fn set_device() -> Result<(), Box<dyn Error>> {
 		"midir-read-input",
 		move |stamp, message, _| {
 			if message[0] == 144 && message[1] == 50 {
-				// next();
+				out_put_device.play_hige_tom();
 			} else if message[0] == 144 && message[1] == 48 {
 				// prev();
 			}
@@ -75,3 +96,5 @@ fn set_device() -> Result<(), Box<dyn Error>> {
 	println!("Closing connection");
 	Ok(())
 }
+
+fn play() {}
